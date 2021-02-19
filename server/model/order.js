bfece821e9ee3ad8d11e011bc567b4470290a703
aksa-urooj <aksaurooj62@gmail.com>
2021-02-19 16:22:50 +0530

    order_model_added

---
+++ server/model/order.js
@@ -0,0 +1,29 @@
+const sequelize = require("./../database/connection");
+const Sequelize = require("sequelize");
+module.exports = sequelize.define(
+  "order",
+  {
+    id: {
+      type: Sequelize.BIGINT(20),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: Sequelize.BIGINT(20),
+      allowNull: false,
+    },
+    event_id: {
+      type: Sequelize.BIGINT(20),
+      allowNull: false,
+    },
+    order_date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: "order",
+  }
+);
