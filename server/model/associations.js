71b8bce07f75dac65ee029c7d34947a52c8335b1
aksa-urooj <aksaurooj62@gmail.com>
2021-02-22 15:38:27 +0530

    association between question_course created

--- server/model/associations.js
+++ server/model/associations.js
@@ -1,81 +1,80 @@
-const {Sequelize} = require('sequelize');
-const Coursecontent=require('./coursecontent.js');
-const Course=require('./course.js');
-const Courseprogress=require('./courseprogress.js');
-const Discovery = require('./discovery.js');
-const Discoveryanswer = require('./discoveryanswer.js');
-
-
+const { Sequelize } = require("sequelize");
+const Coursecontent = require("./coursecontent.js");
+const Course = require("./course.js");
+const Courseprogress = require("./courseprogress.js");
+const Discovery = require("./discovery.js");
+const Discoveryanswer = require("./discoveryanswer.js");
+const Question = require("./question.js");
+const Option = require("./option.js");
 // One to Many relationship between Course and Coursecontent
-Course.hasMany(Coursecontent,{
-    onDelete : "RESTRICT",
-    onUpdate : "RESTRICT",
-    foreignKey:{
-        name:"course_id",
-        type:Sequelize.BIGINT,
-        allowNull:false,
-    },
+Course.hasMany(Coursecontent, {
+  onDelete: "RESTRICT",
+  onUpdate: "RESTRICT",
+  foreignKey: {
+    name: "course_id",
+    type: Sequelize.BIGINT,
+    allowNull: false,
+  },
 });
 
-Coursecontent.belongsTo(Course,{
-    foreignKey:{
-        name : "course_id",
-    },
+Coursecontent.belongsTo(Course, {
+  foreignKey: {
+    name: "course_id",
+  },
 });
 
 // One to Many relationship between Course and Discovery
 
-Course.hasMany(Discovery,{
-    onDelete : "RESTRICT",
-    onUpdate : "RESTRICT",
-    foreignKey:{
-        name:"course_id",
-        type:Sequelize.BIGINT,
-        allowNull:false,
-    },
+Course.hasMany(Discovery, {
+  onDelete: "RESTRICT",
+  onUpdate: "RESTRICT",
+  foreignKey: {
+    name: "course_id",
+    type: Sequelize.BIGINT,
+    allowNull: false,
+  },
 });
-   
-Discovery.belongsTo(Course,{
-    foreignKey:{
-        name : "course_id",
-    },
+
+Discovery.belongsTo(Course, {
+  foreignKey: {
+    name: "course_id",
+  },
 });
 
 // One to Many relationship between Coursecontent and Courseprogress
 
-Coursecontent.hasMany(Courseprogress,{
-    onDelete : "RESTRICT",
-    onUpdate : "RESTRICT",
-    foreignKey:{
-        name:"course_content_id",
-        type:Sequelize.BIGINT,
-        allowNull:false,
-    },
+Coursecontent.hasMany(Courseprogress, {
+  onDelete: "RESTRICT",
+  onUpdate: "RESTRICT",
+  foreignKey: {
+    name: "course_content_id",
+    type: Sequelize.BIGINT,
+    allowNull: false,
+  },
 });
 
-Courseprogress.belongsTo(Coursecontent,{
-    foreignKey:{
-        name : "course_content_id",
-    },
+Courseprogress.belongsTo(Coursecontent, {
+  foreignKey: {
+    name: "course_content_id",
+  },
 });
-
 
 // One to Many relationship between Discovery and Discoveryanswer
 
-Discovery.hasMany(Discoveryanswer,{
-    onDelete : "RESTRICT",
-    onUpdate : "RESTRICT",
-    foreignKey:{
-        name:"discovery_id",
-        type:Sequelize.BIGINT,
-        allowNull:false,
-    },
+Discovery.hasMany(Discoveryanswer, {
+  onDelete: "RESTRICT",
+  onUpdate: "RESTRICT",
+  foreignKey: {
+    name: "discovery_id",
+    type: Sequelize.BIGINT,
+    allowNull: false,
+  },
 });
 
-Discoveryanswer.belongsTo(Discovery,{
-    foreignKey:{
-        name : "discovery_id",
-    },
+Discoveryanswer.belongsTo(Discovery, {
+  foreignKey: {
+    name: "discovery_id",
+  },
 });
 
 //Association between table Question and table Option
@@ -91,5 +90,21 @@ Question.hasMany(Option, {
 Option.belongsTo(Question, {
   foreignKey: {
     name: "question_id",
+  },
+});
+
+//Association between table Course and table Question
+Course.hasMany(Question, {
+  onDelete: "RESTRICT",
+  onUpdate: "RESTRICT",
+  foreignKey: {
+    name: "course_id",
+    type: DataTypes.BIGINT(20),
+    allowNull: false,
+  },
+});
+Question.belongsTo(Course, {
+  foreignKey: {
+    name: "course_id",
   },
 });
